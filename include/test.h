817f78ff077d155b092c5519e4cdbbfee3b7557e
Michael Rahr <michael.rahr@gmil.com>
2025-03-13 12:03:51 +0100

    Adding mutiple and single cell defines

--- include/test.h
+++ include/test.h
@@ -5,12 +5,23 @@
 #define LED4                     GPIO_NUM_12
 #define LED5                     GPIO_NUM_13
 
+#define singlecell //multiplecell 
+
+#ifdef multiplecell
 #define battLevel1                2.6
 #define battLevel2                2.7
 #define battLevel3                2.8
 #define battLevel4                2.9
 #define battLevel5                3.0
+#endif
 
+#ifdef singlecell
+#define battLevel1                2.9
+#define battLevel2                3.1
+#define battLevel3                3.5
+#define battLevel4                3.9
+#define battLevel5                4.1
+#endif
 
 
 void setPin(int pinnumber);
